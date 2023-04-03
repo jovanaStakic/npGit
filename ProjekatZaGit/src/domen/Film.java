f05536b55d94138e3930178364e01807b1536e60
Jovana Stakić <jovana.stakic997@gmail.com>
2023-04-03 21:34:56 +0200

    Cetvrti komit

--- ProjekatZaGit/src/domen/Film.java
+++ ProjekatZaGit/src/domen/Film.java
@@ -1,5 +1,20 @@
 package domen;
 
 public class Film {
-
+	String naziv;
+	int godinaIzlaska;
+	public String getNaziv() {
+		return naziv;
+	}
+	public void setNaziv(String naziv) {
+		this.naziv = naziv;
+	}
+	public int getGodinaIzlaska() {
+		return godinaIzlaska;
+	}
+	public void setGodinaIzlaska(int godinaIzlaska) {
+		this.godinaIzlaska = godinaIzlaska;
+	}
+	
+	
 }
